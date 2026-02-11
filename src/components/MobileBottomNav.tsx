b9b111eb28ef4b9ec5a673e28d9999b2fc1de8a5
yenkj <49618620+yenkj@users.noreply.github.com>
2026-02-11 16:22:40 +0800

    Update MobileBottomNav.tsx

--- src/components/MobileBottomNav.tsx
+++ src/components/MobileBottomNav.tsx
@@ -2,7 +2,7 @@
 
 'use client';
 
-import { Box, Cat, Clover, Film, Globe, Home, PlaySquare, Radio, Star, Tv } from 'lucide-react';
+import { Box, Cat, Clover, Film, FolderOpen, Globe, Home, PlaySquare, Radio, Star, Tv } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useCallback, useEffect, useRef, useState } from 'react';
